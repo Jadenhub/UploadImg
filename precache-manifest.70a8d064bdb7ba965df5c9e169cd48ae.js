90ceadf427da8890a4f79bd972f66c033ff3d09c
Roy <cjh19891018@gmail.com>
2019-12-25 13:21:29 +0800

    Updates

---
+++ precache-manifest.70a8d064bdb7ba965df5c9e169cd48ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a27a8d6886458a296fab8f78a810130",
+    "url": "/uploadimg/index.html"
+  },
+  {
+    "revision": "24760927276c36f92174",
+    "url": "/uploadimg/static/css/main.0796b866.chunk.css"
+  },
+  {
+    "revision": "5bfa42bdf56e2784fb7b",
+    "url": "/uploadimg/static/js/2.83cf591b.chunk.js"
+  },
+  {
+    "revision": "6207cc49c334a88845df4e47971d5513",
+    "url": "/uploadimg/static/js/2.83cf591b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24760927276c36f92174",
+    "url": "/uploadimg/static/js/main.3352541f.chunk.js"
+  },
+  {
+    "revision": "b73e69897a019bc7be8b",
+    "url": "/uploadimg/static/js/runtime-main.8bb0142a.js"
+  }
+]);
